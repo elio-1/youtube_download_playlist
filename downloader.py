10c505e0b2284c51507b331561c427cdefeb4f2e
Elio Raphael <69924027+elio75006@users.noreply.github.com>
2022-12-09 11:04:40 +0100

    added cli

--- downloader.py
+++ downloader.py
@@ -1,13 +1,34 @@
 from pytube import YouTube, Playlist
+import os
+from pathlib import Path
+import sys
+import re
 
-def Dlink(link, res="360p"):
-    stream = YouTube(link).streams.filter(res=res).first()
-    return stream
 
 def Dplaylist(link, res="360p"):
     p = Playlist(link)
+    title = p.title.replace(":", "")
+    output_path = str(Path.home() / "Downloads" / title)
+    os.mkdir(output_path)
     for video in p.videos:
-        video.streams.filter(res=res).first().download()
+        video.streams.filter(res=res).first().download(output_path=output_path)
 
-# print(Dlink("https://www.youtube.com/watch?v=DEHsvQ3Ylwg&list=PL8dPuuaLjXtNlUrzyH5r6jN9ulIgZBpdo&index=40"))
-Dplaylist("https://www.youtube.com/playlist?list=PLfsEr6YiELADnmHqadhdZTBUngZfEMgpz")
+
+def Dlink(link, res="360p"):
+    yt = YouTube(link)
+    output_path = str(Path.home() / "Downloads")
+    yt.streams.filter(res=res).first().download(output_path=output_path)
+
+
+link = input("Link: ")
+if len(sys.argv) == 2:
+    res = sys.argv[1]
+else:
+    res = "360p"
+
+is_playlist = re.search("list", link) != None
+
+if is_playlist:
+    Dplaylist(link, res)
+else:
+    Dlink(link, res)
