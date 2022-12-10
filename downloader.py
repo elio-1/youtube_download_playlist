673bbe9582cc829af887c66c30fa806e70b2bb3f
Elio Raphael <69924027+elio75006@users.noreply.github.com>
2022-12-10 17:51:00 +0100

    add download bar

--- downloader.py
+++ downloader.py
@@ -1,21 +1,26 @@
 from pytube import YouTube, Playlist
+from pytube.cli import on_progress
 import os
 from pathlib import Path
 import sys
 import re
 
-
 def Dplaylist(link, res="360p"):
     p = Playlist(link)
     title = p.title.replace(":", "")
     output_path = str(Path.home() / "Downloads" / title)
-    os.mkdir(output_path)
-    for video in p.videos:
-        video.streams.filter(res=res).first().download(output_path=output_path)
+    try:
+        os.mkdir(output_path)
+    except FileExistsError: 
+        pass
+    video_urls = p.video_urls
+    for video in video_urls:
+        yt = YouTube(video, on_progress_callback=on_progress)
+        yt.streams.filter(res=res).first().download(output_path=output_path)
 
 
 def Dlink(link, res="360p"):
-    yt = YouTube(link)
+    yt = YouTube(link, on_progress_callback=on_progress)
     output_path = str(Path.home() / "Downloads")
     yt.streams.filter(res=res).first().download(output_path=output_path)
 
